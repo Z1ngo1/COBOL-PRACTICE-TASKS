022bd4a16bb71c9b63130de769976215277373bc
STANISLAW <stas_dmitriev_2007@bk.ru>
2026-04-15 05:31:30 +0500

    Create INSERT.DATA.sql

---
+++ TASKS/TASK26-DB2-VSAM-PAYMENT-BATCH/SQL/INSERT.DATA.sql
@@ -0,0 +1,10 @@
+-- TASK 26: Robust Batch Processing
+
+INSERT INTO TB_CUSTOMER_BALANCE (CUST_ID,CUST_BALANCE)
+VALUES ('00100',10000);                                                                    
+INSERT INTO TB_CUSTOMER_BALANCE (CUST_ID,CUST_BALANCE)
+VALUES ('00200',5000);                                                                                     
+INSERT INTO TB_CUSTOMER_BALANCE (CUST_ID,CUST_BALANCE)
+VALUES ('00300',2000);                                                                                      
+INSERT INTO TB_CUSTOMER_BALANCE (CUST_ID,CUST_BALANCE)
+VALUES ('00400',15000);                               
